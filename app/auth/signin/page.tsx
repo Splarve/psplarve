900da87e49d4c0de77cf28c5b2c7fd31f82fc4c5
EnesAkyuz <eakyuz0681@gmail.com>
2025-04-10 01:29:54 -0700

    cleanup

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,12 +1,14 @@
 // app/auth/signin/page.tsx
 import SignInForm from '@/components/auth/SignInForm'
+import AuthButton from '@/components/auth/AuthButton'
 
 export default function SignIn() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="flex flex-col items-center justify-center flex-1 px-4 sm:px-20">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">      
+      {/* Keep the form for functionality, but you can style it to be less prominent */}
+      <div className="mt-20">
         <SignInForm />
-      </main>
+      </div>
     </div>
   )
 }
